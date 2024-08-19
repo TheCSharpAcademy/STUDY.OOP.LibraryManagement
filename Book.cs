bd5774d5b4151e981404a760f6755c8c54c0818c
Pablo <thecsharpacademy@gmail.com>
2024-08-19 21:06:07 +1000

    Book model

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿namespace STUDY.OOP.LibraryManagement;
+
+internal class Book
+{
+    string Name;
+    string Location;
+
+    internal Book()
+    {
+        Console.WriteLine($"Book name and location are unknown");
+    }
+
+    internal Book(string name)
+    {
+        Name = name;
+        Console.WriteLine($"Book name = {name} and location is unknown");
+    }
+
+    internal Book(string name, string location)
+    {
+        Name = name;
+        Location = location;
+
+        Console.WriteLine($"Book name = {name} and location is {location}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,24 +2,9 @@
 
 // UserInterface.MainMenu();
 
-
-Car car1 = new Car();
-Car car2 = new Car();
-
-car1.name = "Ferrari";
-car2.name = "BMW";
-
-car1.PrintName();
-car2.PrintName();
-
-class Car()
-{
-    internal string name;
-
-    internal void PrintName()
-    {
-        Console.WriteLine($"I'm a {name}");
-    }
-}
+var book1 = new Book("Godfather");
+var book2 = new Book("Pride and Prejudice");
+var book3 = new Book();
+var book4 = new Book("Harry Potter", "A43");
 
 
